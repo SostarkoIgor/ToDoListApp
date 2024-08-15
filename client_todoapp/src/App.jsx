0ab47b237e9f33f15a9ed6efa6ddd4e34030a5b3
Igor Sostarko <igor.sostarko@fer.hr>
2024-08-16 00:54:42 +0200

    frontend auth component made, added jsx pages without content

--- client_todoapp/src/App.jsx
+++ client_todoapp/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import { useNavigate, BrowserRouter, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Authorize from './auth/Authorize'
 
 function App() {
 
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    
+
+  }, [])
+
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Authorize><Home /></Authorize>} />
+    </BrowserRouter>
   )
 }
 
---
+++ client_todoapp/src/auth/Authorize.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+
+function isValidToken(token) {
+    try{
+    const decoded = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decoded.exp > currentTime;
+    } catch (e) {
+        return false;
+    }
+}
+
+function Authorize({ children }) {
+  const token = localStorage.getItem('token');
+
+  if (token && isValidToken(token))
+    return children
+  else{
+    localStorage.removeItem('token')
+    return <Navigate to="/login" />
+  }
+}
+
+export default Authorize;
