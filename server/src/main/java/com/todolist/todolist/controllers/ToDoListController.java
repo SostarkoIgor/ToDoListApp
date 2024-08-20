387956b980eab593c4b2214250c4ed327b707ee2
Igor Sostarko <igor.sostarko@fer.hr>
2024-08-21 01:28:04 +0200

    implemented controller delete put get and post methods

--- server/src/main/java/com/todolist/todolist/controllers/ToDoListController.java
+++ server/src/main/java/com/todolist/todolist/controllers/ToDoListController.java
@@ -4,14 +4,15 @@ import com.todolist.todolist.auth.AuthenticationService;
 import com.todolist.todolist.dtos.TaskDto;
 import com.todolist.todolist.dtos.ToDoListCreateDto;
 import com.todolist.todolist.dtos.ToDoListResponseDto;
+import com.todolist.todolist.dtos.UserListDto;
 import com.todolist.todolist.models.ToDoList;
-import com.todolist.todolist.repos.ToDoListRepository;
 import com.todolist.todolist.services.TaskService;
 import com.todolist.todolist.services.ToDoListService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.http.ResponseEntity;
@@ -31,7 +32,7 @@ public class ToDoListController {
             return ResponseEntity.notFound().build();
         }
         if (list.getOwner()!=authenticationService.getCurrentlyLoggedInUser()){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         return ResponseEntity.ok().body(ToDoListResponseDto.builder()
                 .listname(list.getListName())
@@ -48,10 +49,42 @@ public class ToDoListController {
     public ResponseEntity<ToDoListCreateDto> createList(@RequestBody ToDoListCreateDto toDoListCreateDto){
         var list=toDoListService.createList(toDoListCreateDto.getListname(), authenticationService.getCurrentlyLoggedInUser());
         for (var l : toDoListCreateDto.getTasks()){
-            taskService.addTask(l, list);
+            taskService.addTask(l, list, false);
         }
         return ResponseEntity.ok(toDoListCreateDto);
     }
 
+    @GetMapping("/user")
+    public ResponseEntity<Page<UserListDto>> getUserLists(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ){
+        var currentUser=authenticationService.getCurrentlyLoggedInUser();
+        return ResponseEntity.ok(toDoListService.findAllListsOfUser(currentUser, PageRequest.of(page, size)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteList(@PathVariable int id){
+        var list= toDoListService.getById(id);
+        if (list==null) return ResponseEntity.notFound().build();
+        if (!authenticationService.getCurrentlyLoggedInUser().getId().equals(list.getOwner().getId())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        toDoListService.deleteListById(list.getId());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<ToDoListResponseDto> updateList(@RequestBody ToDoListResponseDto listDto){
+        var list=toDoListService.getById(listDto.getId());
+        if (list==null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (!list.getOwner().getId().equals(authenticationService.getCurrentlyLoggedInUser().getId())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        list.setListName(listDto.getListname());
+        taskService.updateListTasks(list, listDto.getTasks());
+        toDoListService.saveList(list);
+        return ResponseEntity.ok(listDto);
+    }
 
 }
